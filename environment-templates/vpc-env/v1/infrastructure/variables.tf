a7b1868bdff07fa59a7a13ea20091bba69684473
mohsinsheikhani <imohsinsheikhani@gmail.com>
2025-07-28 18:08:29 +0500

    spec

--- environment-templates/vpc-env/v1/infrastructure/variables.tf
+++ environment-templates/vpc-env/v1/infrastructure/variables.tf
@@ -4,6 +4,13 @@ variable "environment" {
       vpc_name = string
       vpc_cidr = string
     })
+    name = string
   })
   description = "Environment inputs from Proton"
+}
+
+variable "proton_tags" {
+  type        = map(string)
+  description = "Tags provided by Proton"
+  default     = {}
 }
